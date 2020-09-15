26f91ddae3ee06886497be9257cc932c15af5724
wtf <593616590@qq.com>
2020-09-15 08:23:43 +0800

    新增生成Dao功能

--- src/main/java/com/wtf/tool/util/generator/creator/CustomPlugin.java
+++ src/main/java/com/wtf/tool/util/generator/creator/CustomPlugin.java
@@ -4,10 +4,7 @@ import com.wtf.tool.util.generator.GenCommentGenerator;
 import com.wtf.tool.util.generator.creator.core.SqlCreatorComposite;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
-import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
-import org.mybatis.generator.api.dom.java.Interface;
-import org.mybatis.generator.api.dom.java.Method;
-import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.java.*;
 import org.mybatis.generator.api.dom.xml.Document;
 import org.mybatis.generator.api.dom.xml.XmlElement;
 import org.mybatis.generator.config.CommentGeneratorConfiguration;
@@ -40,6 +37,10 @@ public class CustomPlugin extends PluginAdapter {
     @Override
     public void setProperties(Properties properties) {
         super.setProperties(properties);
+        String mappers = this.properties.getProperty("mappers");
+        for (String mapper : mappers.split(",")) {
+            this.mappers.add(mapper);
+        }
     }
 
     @Override
@@ -48,17 +49,27 @@ public class CustomPlugin extends PluginAdapter {
     }
 
     @Override
-    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable table) {
         // 获取实体类
-        entityType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+        entityType = new FullyQualifiedJavaType(table.getBaseRecordType());
         // import接口
-        for (String mapper : mappers) {
-            interfaze.addImportedType(new FullyQualifiedJavaType(mapper));
-            interfaze.addSuperInterface(new FullyQualifiedJavaType(mapper + "<" + entityType.getShortName() + ">"));
-        }
+//        for (String mapper : mappers) {
+//            interfaze.addImportedType(new FullyQualifiedJavaType(mapper));
+//            interfaze.addSuperInterface(new FullyQualifiedJavaType(mapper + "<" + entityType.getShortName() + ">"));
+//        }
+        Method method = new Method();
+        method.setName("count");
+        method.addParameter(new Parameter(entityType, entityType.getShortName()));
+        method.setReturnType(new FullyQualifiedJavaType("Long"));
+        interfaze.addMethod(method);
+        Method method1 = new Method();
+        method1.setName("countLike");
+        method1.addParameter(new Parameter(entityType, entityType.getShortName()));
+        method1.setReturnType(new FullyQualifiedJavaType("Long"));
+        interfaze.addMethod(method1);
         // import实体类
         interfaze.addImportedType(entityType);
-        return true;
+        return super.clientGenerated(interfaze, topLevelClass, table);
     }
 
     @Override
@@ -87,6 +98,9 @@ public class CustomPlugin extends PluginAdapter {
     // 以下设置为false,取消生成默认增删查改xml
     @Override
     public boolean clientDeleteByPrimaryKeyMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
+//        method.setName("count");
+//        method.setReturnType(FullyQualifiedJavaType.getIntInstance());
+//        interfaze
         return false;
     }
 
